2f9900773882ae7db3af44ba77161a188c21ad7e
Hezron-tech <kiprophez@gmail.com>
2022-03-25 17:35:25 +0300

    adding class models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,20 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=100,null=False,blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    category =models.ForeignKey(category,on_delete=models.SET_NULL) 
+    description=models.CharField(max_length=100,null=False,blank=False)
+
+    def __str__(self):
+        return self.name        
+
