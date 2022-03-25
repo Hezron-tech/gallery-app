ac1646ff62490b914a36f3a0c63037b019d9f4fd
Hezron-tech <kiprophez@gmail.com>
2022-03-25 14:51:48 +0300

    adding views function

--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def gallery(request):
+    return render(request, 'photos/gallery.html')
+
+
+def gallery(request):
+     return render(request, 'photos/gallery.html')
+
+
+def gallery(request):
+     return render(request, 'photos/gallery.html')
