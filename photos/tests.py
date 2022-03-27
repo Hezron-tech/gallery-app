4c02792560e2470af0a72ba74c275848b0ee0919
Hezron-tech <kiprophez@gmail.com>
2022-03-27 21:27:47 +0300

    running tes

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,15 @@
+from unicodedata import category
 from django.test import TestCase
 
+from photos.models import Category
+
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.travel= Category(category_name='Travel')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category))    
